fbfd8907b9a4ca66c5b2ee2756423c9abfe2a85a
Sven Olding <sven.olding@gmx.de>
2024-02-13 14:05:22 +0100

    init spring boot project

---
+++ src/main/java/com/example/bambootest/BambooTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bambootest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bambootest/BambooTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bambootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
